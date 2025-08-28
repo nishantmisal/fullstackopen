2cee9fd3b6ff285962e558c886cc6de976a720c7
nishantmisal <nishantmisal07@gmail.com>
2025-08-28 16:29:27 -0700

    part 1 directory & Exercise 1.3

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,59 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.name} {props.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]}/>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+    </div>
+  )
+}
+
+export default App
